721b1a2cb8fb4328b434ac48c9c2d6d59599cb7e
Kevindra Singh <kevindra.singh@gmail.com>
2021-03-09 20:54:45 -0800

    deleting build src

--- build/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-const world = 'world';
-function hello(word = world) {
-    return `Hello ${world}! `;
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
